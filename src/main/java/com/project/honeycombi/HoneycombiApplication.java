be76ccfaf01dd8b4dab1bdeb6d4227658712a5d9
PgrmKMB <emdk789@naver.com>
2022-04-07 17:36:49 +0900

    commit

---
+++ src/main/java/com/project/honeycombi/HoneycombiApplication.java
@@ -0,0 +1,13 @@
+package com.project.honeycombi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneycombiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoneycombiApplication.class, args);
+	}
+
+}
